57303448c191302e7231bf97dc496cfae70a2abb
Leonardo Castro <alexchristie1988@gmail.com>
2025-10-02 16:03:32 -0300

    variables de entorno y modelo nuevo

--- src/constants/model-collection.dictionary.ts
+++ src/constants/model-collection.dictionary.ts
@@ -5,5 +5,6 @@ export const MODEL_COLLECTION_DICTIONARY = {
     "Sofia-v0.0.6-deepseek-r1:8b": "sofia-memory-3",
     "Sofia-v0.0.6-deepseek-r1:5b": "sofia-memory-3",
     "Kasumi-v0.0.2-openchat:7b-q3": "kasumi-memory-1",
-    "Kasumi-v0.0.1-openchat:7b-q2": "kasumi-memory-1"
+    "Kasumi-v0.0.1-openchat:7b-q2": "kasumi-memory-1",
+    "Sophia-v0.0.1:latest": "sophia-memory1"
 };
--- src/core/env.validation.ts
+++ src/core/env.validation.ts
@@ -2,5 +2,8 @@ import * as Joi from 'joi'
 
 export const VALIDATION_SCHEMA = Joi.object({
     URL_EMBEDING_MODEL: Joi.string().required(),
-    URL_LLM_MODEL: Joi.string().required()
+    URL_LLM_MODEL: Joi.string().required(),
+    URL_WS: Joi.string().required(),
+    PORT_WS: Joi.number().required(),
+    VTUBE_TOKEN: Joi.string().required(),
 })
