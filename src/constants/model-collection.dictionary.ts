961d8326cd84ddf8831480f7d389420d2f28f34c
Leonardo Castro <alexchristie1988@gmail.com>
2026-02-11 21:54:21 -0300

    test changes

--- src/constants/model-collection.dictionary.ts
+++ src/constants/model-collection.dictionary.ts
@@ -1,3 +1,5 @@
 export const MODEL_COLLECTION_DICTIONARY = {
-    "Kasumi-llama3.2:3b": "kasumi-memory-0"
+    "Kasumi-llama3.2:3b": "kasumi-memory-0",
+    "Kasumi-llama3.2:3b-test": "kasumi-memory-0",
+    "Kasumi-llama3.2:3b-test-1": "kasumi-memory-0"
 };
